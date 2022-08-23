9d4591f72d767024a13dff688b4255ff155c458b
Xander-Trof <lextrofimov98@gmail.com>
2022-08-23 12:52:05 +0300

    Adds save func to forms

--- mybrary_app/forms.py
+++ mybrary_app/forms.py
@@ -1,8 +1,30 @@
 from django import forms
-from mybrary_app.models import Books
+from mybrary_app.models import Books, Authors
+
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model = Authors
+        fields = [
+            'full_name',
+        ]
 
 
 class BooksForm(forms.ModelForm):
     class Meta:
         model = Books
-        exclude = []
+        exclude = [
+            'author',
+        ]
+
+    def save(self, commit=True, author=None):
+        if author is None:
+            raise ValueError('authors name was not set')
+
+        inst = super().save(commit=False)
+        inst.author = author
+
+        if commit:
+            inst.save()
+
+        return inst
