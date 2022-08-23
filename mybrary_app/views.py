b2e8dcb06b6c508a789f7828db28801afd8bc207
Xander-Trof <lextrofimov98@gmail.com>
2022-08-23 12:57:06 +0300

    Adds view for creating new books items

--- mybrary_app/views.py
+++ mybrary_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
+from django.db import transaction
 
-from mybrary_app.models import Books
-from mybrary_app.forms import BooksForm
+from mybrary_app.models import Books, Authors
+from mybrary_app.forms import BooksForm, AuthorForm
 
 # Create your views here.
 
@@ -12,17 +13,26 @@ def index(request):
     if request.method == 'GET':
         books = Books.objects.all()
         return render(request, 'mybrary_app/index.html', {'books': books})
+
     return HttpResponse(status=405)
 
 
 def add_book(request):
     if request.method == 'GET':
-        return render(request, 'mybrary_app/add-book.html', {'book_form': BooksForm()})
+        c = {
+            'book_form': BooksForm(),
+            'author_form': AuthorForm(),
+        }
+        return render(request, 'mybrary_app/add-book.html', c)
 
     elif request.method == 'POST':
         book_form = BooksForm(request.POST)
+        author_form = AuthorForm(request.POST)
 
-        if book_form.is_valid():
-            book = book_form.save()
+        if book_form.is_valid() and author_form.is_valid():
+            with transaction.atomic():
+                author = author_form.save()
+                book = book_form.save(author=author)
+                book_form.save_m2m()
 
-            return redirect(reverse('books:index'))
+            return redirect(reverse('index'))
